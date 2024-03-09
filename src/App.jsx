004dd58b14a998014d742c51134730440acf09d4
Vicente Nava <vicnav28@gmail.com>
2024-03-09 01:25:36 -0600

    using Chakra

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import PageNavigation from './components/PageNavigation';
 
 function App() {
 	return (
-		<div className='flex min-h-screen bg-violet-950'>
-			<div className='flex flex-col items-start space-y-4 bg-black/50 px-5 py-8 text-white'>
-				<PageNavigation />
-			</div>
-			<div className='flex flex-auto p-8'>
-				<TemplateSelector />
-				<div className='flex max-h-screen w-2/3 flex-col items-center justify-start gap-5'>
-					<Visualizer />
+	
+			<div className='flex min-h-screen bg-violet-950'>
+				<div className='flex flex-col items-start space-y-4 bg-black/50 px-5 py-8 text-white'>
+					<PageNavigation />
+				</div>
+				<div className='flex flex-auto p-8'>
+					<TemplateSelector />
+					<div className='flex max-h-screen w-2/3 flex-col items-center justify-start gap-5'>
+						<Visualizer />
+					</div>
 				</div>
 			</div>
-			*/
-		</div>
 	);
 }
 
--- src/components/PageNavigation.jsx
+++ src/components/PageNavigation.jsx
@@ -3,12 +3,29 @@ import { useAtom } from 'jotai';
 import { displayedPageIndexAtom } from '../lib/atoms';
 
 import { PlusCircleIcon, XMarkIcon } from '@heroicons/react/16/solid';
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	AlertDialogCloseButton,
+	Button,
+	useDisclosure,
+} from '@chakra-ui/react';
+import { useRef, useState } from 'react';
 
 export default function PageNavigation() {
 	const [displayedPageIndex, setDisplayedPageIndex] = useAtom(
 		displayedPageIndexAtom,
 	);
 	const [pages, setPages] = useAtom(pagesAtom);
+	const [indexToDelete, setIndexToDelete] = useState(null);
+
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const cancelRef = useRef();
 
 	const handleAddPage = () => {
 		setPages([
@@ -26,23 +43,29 @@ export default function PageNavigation() {
 		]);
 	};
 
-	const handleDeletePage = (e, indexToDelete) => {
+	const confirmDeletePage = (e, indexToDelete) => {
 		e.stopPropagation();
+		setIndexToDelete(indexToDelete);
+		onOpen();
+	};
 
-		const deletePage = () =>
+	const handleDeletePage = () => {
+		const deletePage = () => {
 			setPages(pages.filter((_, index) => index !== indexToDelete));
+
+			if (indexToDelete === displayedPageIndex) {
+				setDisplayedPageIndex(
+					indexToDelete === 0 ? indexToDelete : indexToDelete - 1,
+				);
+			} else if (indexToDelete < displayedPageIndex) {
+				setDisplayedPageIndex(displayedPageIndex - 1);
+			}
+		};
 
 		if (pages.length === 1) {
 			alert('No se puede borrar la única página del libro');
-		} else if (indexToDelete === displayedPageIndex) {
-			setDisplayedPageIndex(
-				indexToDelete === 0 ? indexToDelete : indexToDelete - 1,
-			);
-			deletePage();
-		} else if (indexToDelete < displayedPageIndex) {
-			setDisplayedPageIndex(displayedPageIndex - 1);
-			deletePage();
 		} else {
+			onClose();
 			deletePage();
 		}
 	};
@@ -72,7 +95,7 @@ export default function PageNavigation() {
 				<button
 					className='transtion group flex h-full items-center justify-center bg-red-500 px-2 duration-100 hover:scale-125 hover:cursor-pointer group-hover:ring group-hover:ring-cyan-500'
 					onClick={e => {
-						handleDeletePage(e, index);
+						confirmDeletePage(e, index);
 					}}
 				>
 					<XMarkIcon className='h-8 text-white group-hover:scale-125' />
@@ -93,6 +116,33 @@ export default function PageNavigation() {
 			<span className='w-full text-center font-inter text-2xl font-bold'>
 				{displayedPageIndex + 1} / {pages.length}
 			</span>
+
+			<AlertDialog
+				isOpen={isOpen}
+				leastDestructiveRef={cancelRef}
+				onClose={onClose}
+			>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+							Borrar página
+						</AlertDialogHeader>
+
+						<AlertDialogBody>
+							¿Estás seguro? Esta acción no puede ser revertida.
+						</AlertDialogBody>
+
+						<AlertDialogFooter>
+							<Button ref={cancelRef} onClick={onClose}>
+								Cancelar
+							</Button>
+							<Button colorScheme='red' onClick={handleDeletePage} ml={3}>
+								Borrar página
+							</Button>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
 		</>
 	);
 }
